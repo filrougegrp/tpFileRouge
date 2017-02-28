ac29eeada9132522d7479bdd10a8601787e06647
SAOUD Mohamed Amine <masaoud@norsys.fr>
2017-02-28 17:43:44 +0000

    add sql requete

--- src/test/java/resources/db/requetes.sql
+++ src/test/java/resources/db/requetes.sql
@@ -1,1 +1,17 @@
 --init requetes
+
+
+
+
+
+
+
+
+
+
+
+-- select les pays de tous les poules
+select po.NOM, pa.NOM from POULE po 
+	inner join POULE_PAYS pp on po.ID_POULE = pp.ID_POULE 
+	inner join PAYS pa on pa.ID_PAYS = pp.ID_PAYS;
+	
