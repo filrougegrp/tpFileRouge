98244b9511dc60ed8549630715b237acdba4b8f6
marouaneunix <elmerrouni.marouane@gmail.com>
2017-02-28 16:50:12 +0000

    Insert pronostic for salarie 2 and 3

--- src/main/java/org/sinaf/tpFileRouge/App.java
+++ src/main/java/org/sinaf/tpFileRouge/App.java
@@ -11,9 +11,9 @@ public class App {
 
 		Random rn = new Random();
 
-		for (int i = 2; i <= 24; i++) {
+		for (int i = 1; i <= 24; i++) {
 			System.out.println("INSERT INTO `PRONOSTIC` (`BUT_1`, `BUT_2`, `NOTE`, `ID_MATCH`, `ID_SALARIE`) VALUES ('"
-					+ rn.nextInt(6 - 0 + 1) + "', '" + rn.nextInt(6 - 0 + 1) + "', '0', '" + i + "', '3');");
+					+ rn.nextInt(6 - 0 + 1) + "', '" + rn.nextInt(6 - 0 + 1) + "', '0', '" + i + "', '1');");
 		}
 	}
 }
