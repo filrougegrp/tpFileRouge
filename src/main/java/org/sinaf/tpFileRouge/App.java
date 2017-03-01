4fd18fbd048c638d2f08211caa5924069e18b391
marouaneunix <elmerrouni.marouane@gmail.com>
2017-03-01 08:57:13 +0000

    update RENCONTRE TABLE

--- src/main/java/org/sinaf/tpFileRouge/App.java
+++ src/main/java/org/sinaf/tpFileRouge/App.java
@@ -11,9 +11,17 @@ public class App {
 
 		Random rn = new Random();
 
-		for (int i = 1; i <= 24; i++) {
-			System.out.println("INSERT INTO `PRONOSTIC` (`BUT_1`, `BUT_2`, `NOTE`, `ID_MATCH`, `ID_SALARIE`) VALUES ('"
-					+ rn.nextInt(6 - 0 + 1) + "', '" + rn.nextInt(6 - 0 + 1) + "', '0', '" + i + "', '1');");
+		// for (int i = 1; i <= 24; i++) {
+		// System.out.println("INSERT INTO `PRONOSTIC` (`BUT_1`, `BUT_2`,
+		// `NOTE`, `ID_MATCH`, `ID_SALARIE`) VALUES ('"
+		// + rn.nextInt(6 - 0 + 1) + "', '" + rn.nextInt(6 - 0 + 1) + "', '0',
+		// '" + i + "', '1');");
+		// }
+
+		for (int i = 2; i <= 24; i++) {
+			System.out.println("UPDATE RENCONTRE SET BUT_1=" + rn.nextInt(6 - 0 + 1) + ",BUT_2=" + rn.nextInt(6 - 0 + 1)
+					+ " WHERE ID_RENCONTRE = " + i + ";");
 		}
+
 	}
 }
